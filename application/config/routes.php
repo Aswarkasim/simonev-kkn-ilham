fd9f3937bd5a39dcd1efcb9dd0e6912d887eca76
aswarkasim <aswarkasim@gmail.com>
2021-10-24 13:39:23 +0700

    route

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['default_controller'] = 'admin/dashboard';
+$route['default_controller'] = 'admin/auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
